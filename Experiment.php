4b74d67325ded8d0cf5c82538ee185f4c8aaeaf7
yohantoro <yohantoro.prakasita@gmail.com>
2017-02-02 09:44:15 +0700

    Experiment class

---
+++ Experiment.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace yohantoro\sesuatu;
+
+/**
+ * This is just an example.
+ */
+class Experiment extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
